f4a88ef1b7d97abb2be1659c4152e5b2f57124fd
emiemiemi <emijoaco_pereyra@hotmail.com>
2024-12-11 20:07:42 -0300

    Implementacion del if

--- Tarea_Moodle/code/ex2.c
+++ Tarea_Moodle/code/ex2.c
@@ -25,7 +25,7 @@ __asm volatile(
 "		LD.D	$w7,0(%[Rthree])	\n"
 "		LD.D	$w8,0(%[Rfive])		\n"
 // YOUR CODE
-"       li		$4, 0x800			\n" // Numero total de elementos
+"       li		$4, 0x800			\n" // Numero total de elementos en bytes (2048 = 256*8*64)
 "		li		$5, 0x10 			\n" // Moverse de a 2 elementos porque es lo que entra en los registros vectoriales de MSA de 128 bits
 "		move	$6, %[Ra]			\n" // Mover el puntero al array a
 "		move	$7, %[Rb]			\n" // Mover el puntero al array b
@@ -34,14 +34,23 @@ __asm volatile(
 
 "loop:	LD.D	$w0, 0($7)			\n" // Cargar 2 elementos de b
 "       LD.D    $w1, 0($8)           \n" // Cargar 2 elementos de c
-"       FADD.D  $w3, $w0, $w1       \n" // w3 <- b[i] + c[i]
+"       FADD.D  $w3, $w0, $w1       \n" // w3 <- b[i] + c[i] 
 "       st.D    $w3, 0($6)          \n" // a[i] <- b[i] + c[i]
-"       SUB     $4, $4, $5          \n"
-"       DADD    $6, $6, $5          \n"
+
+"       LD.D    $w2, 0($9)          \n" // Cargar 2 elementos de d
+"       FMUL.D  $w4, $w3, $w7       \n" //  w4 <- A*3 (A is w3)
+"       CEQ.D   $w6, $w0, $w1       \n" // A = B?
+"       BSEL.V  $w6, $w4, $w2       \n" // Si a[i] = b[i] copio desde w4, si no desde el d[i] original
+"       st.D    $w6, 0($9)          \n" // Store updated d[i]
+
+
+"       SUB     $4, $4, $5          \n" // Decrementar con los restantes usados
+"       DADD    $6, $6, $5          \n" // Mover las direcciones base
 "       DADD    $7, $7, $5          \n"
 "       DADD    $8, $8, $5          \n"
 "       DADD    $9, $9, $5          \n"
 "       bnez    $4, loop            \n"
+"       nop                         \n"
 
 
 
